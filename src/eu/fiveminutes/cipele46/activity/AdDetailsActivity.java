59a73eb4a3af5c1a1c783f43116977571431c25c
Tomislav Novoselec <tomislav.novoselec@fiveminutes.eu>
2013-07-06 11:08:54 +0200

    ad details added

--- src/eu/fiveminutes/cipele46/activity/AdDetailsActivity.java
+++ src/eu/fiveminutes/cipele46/activity/AdDetailsActivity.java
@@ -1,5 +1,36 @@
 package eu.fiveminutes.cipele46.activity;
 
-public class AdDetailsActivity {
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+import eu.fiveminutes.cipele46.R;
+import eu.fiveminutes.cipele46.fragment.AdDetailsFragment;
+
+public class AdDetailsActivity extends SherlockFragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
+		AdDetailsFragment adf = new AdDetailsFragment();
+		getSupportFragmentManager().beginTransaction().replace(android.R.id.content, adf).commit();
+
+		ActionBar ab = getSupportActionBar();
+		if (ab != null) {
+			ab.setDisplayHomeAsUpEnabled(true);
+			ab.setTitle(R.string.ad_details);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == android.R.id.home) {
+			finish();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
