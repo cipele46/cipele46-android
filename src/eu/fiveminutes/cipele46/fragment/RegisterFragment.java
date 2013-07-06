bbf44cfa53bdc4c7262be7fb8979b61e902d812b
Tomislav Homan <tomislav.homan@fiveminutes.eu>
2013-07-06 17:58:23 +0200

    Collect data from register form and send it to api

--- src/eu/fiveminutes/cipele46/fragment/RegisterFragment.java
+++ src/eu/fiveminutes/cipele46/fragment/RegisterFragment.java
@@ -1,38 +1,29 @@
 package eu.fiveminutes.cipele46.fragment;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
 import eu.fiveminutes.cipele46.R;
 import eu.fiveminutes.cipele46.activity.UserSettingsActivity;
 import eu.fiveminutes.cipele46.activity.UserSettingsActivity.UserSettingsScreen;
+import eu.fiveminutes.cipele46.api.CipeleAPI;
+import eu.fiveminutes.cipele46.api.UserRegistrationListener;
+import eu.fiveminutes.cipele46.model.User;
 
 public class RegisterFragment extends SherlockFragment{
 
-	// Values for email and password at the time of the login attempt.
-	private String mEmail;
-	private String mPassword;
-
 	// UI references.
-	private EditText mEmailView;
-	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
+	private EditText mTxtName;
+	private EditText mTxtEmail;
+	private EditText mTxtPhone;
+	private EditText mTxtPassword;
+	private EditText mTxtPasswordRepeated;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,14 +35,48 @@ public class RegisterFragment extends SherlockFragment{
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		
-
+		mTxtName = (EditText)view.findViewById(R.id.txtName);
+		mTxtEmail = (EditText)view.findViewById(R.id.txtEmail);
+		mTxtPhone = (EditText)view.findViewById(R.id.txtPhone);
+		mTxtPassword = (EditText)view.findViewById(R.id.txtPassword);
+		mTxtPasswordRepeated = (EditText)view.findViewById(R.id.txtPasswordRepeated);
+		
 		view.findViewById(R.id.btn_register).setOnClickListener(
 			new View.OnClickListener() {
 				@Override
 				public void onClick(View view) {
-					//Pošalji podatke na server;
-					startActivity(UserSettingsActivity.buildIntent(getActivity(), UserSettingsScreen.USER_DETAILS));
+					String password = mTxtPassword.getText().toString();
+					String passwordRepeated = mTxtPasswordRepeated.getText().toString();
+					
+					if(password.equals(passwordRepeated) == false) {
+						return;
+					}
+					
+					final User user = collectUserData();
+					
+					CipeleAPI.get().registerUser(user.getName(), user.getEmail(), user.getPhone(), password, 
+							new UserRegistrationListener() {
+								
+								@Override
+								public void onSuccess() {
+									User.setUserAsActive(getActivity(), user);
+									startActivity(UserSettingsActivity.buildIntent(getActivity(), UserSettingsScreen.USER_DETAILS));
+								}
+								
+								@Override
+								public void onFailure(Throwable t) {
+									Toast.makeText(getActivity(), "There was an error during registration", Toast.LENGTH_LONG).show();
+								}
+							});
 				}
 			});
+	}
+	
+	public User collectUserData() {
+		User user = new User();
+		user.setName(mTxtName.getText().toString());
+		user.setEmail(mTxtEmail.getText().toString());
+		user.setPhone(mTxtPhone.getText().toString());
+		return user;
 	}
 }
--- src/eu/fiveminutes/cipele46/model/User.java
+++ src/eu/fiveminutes/cipele46/model/User.java
@@ -1,5 +1,10 @@
 package eu.fiveminutes.cipele46.model;
 
+import eu.fiveminutes.cipele46.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
 public class User {
 
 	private String name;
@@ -24,10 +29,42 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	
 	public String toString() {
 		return name + ", " + email + ", " + phone;
 	}
 	
+	public static void setUserAsActive(Context context, User user) {
+		SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.shared_preferences_name), 0);
+		
+		Editor editor = prefs.edit();
+		editor.putString(context.getString(R.string.preference_user_name), user.getName());
+		editor.putString(context.getString(R.string.preference_user_email), user.getEmail());
+		editor.putString(context.getString(R.string.preference_user_phone), user.getPhone());
+		editor.commit();	
+	}
+	
+	public static void deactivateUser(Context context) {
+		SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.shared_preferences_name), 0);
+		
+		Editor editor = prefs.edit();
+		editor.putString(context.getString(R.string.preference_user_name), "");
+		editor.putString(context.getString(R.string.preference_user_email), "");
+		editor.putString(context.getString(R.string.preference_user_phone), "");
+		editor.commit();	
+	}
+	
+	public static User getActiveUser(Context context) {
+		SharedPreferences prefs = context.getSharedPreferences(context.getString(R.string.shared_preferences_name), 0);
+		
+		if(prefs.getString(context.getString(R.string.preference_user_name), "").equals("")) {
+			return null;
+		}
+		
+		User user = new User();
+		user.setName(prefs.getString(context.getString(R.string.preference_user_name), ""));
+		user.setEmail(prefs.getString(context.getString(R.string.preference_user_email), ""));
+		user.setPhone(prefs.getString(context.getString(R.string.preference_user_phone), ""));
+		return user;
+	}
 }
