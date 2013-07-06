a2311411f4875472418642e06e0f381c29416dc1
Davor Kus <kusdavor@gmail.com>
2013-07-06 11:07:09 +0200

    categories api

--- src/eu/fiveminutes/cipele46/api/CipeleAPI.java
+++ src/eu/fiveminutes/cipele46/api/CipeleAPI.java
@@ -1,8 +1,12 @@
 package eu.fiveminutes.cipele46.api;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.util.Log;
@@ -46,7 +50,30 @@ public class CipeleAPI {
 			@Override
 			public void onResponse(JSONArray response) {
 				Log.i(TAG, response.toString());
-				cl.onSuccess(parseJSONArray(response), null);
+				
+				List<Category> listofCategory = Collections.<Category>emptyList();
+				
+				for (int i  =0; i < response.length(); i++) {
+					
+					if (listofCategory.isEmpty()) {
+						listofCategory = new ArrayList<Category>();
+					}
+					
+					JSONObject jsonObject;
+					try {
+						
+						jsonObject = response.getJSONObject(i);
+						Category category = new Category();
+						category.setId(jsonObject.getString("id"));
+						category.setName(jsonObject.getString("name"));
+						listofCategory.add(category);
+						
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				cl.onSuccess(listofCategory, null);
 				
 			}
 		}, new ErrorListener() {
@@ -62,11 +89,5 @@ public class CipeleAPI {
 		reqQueue.add(jsonArrayRequest);
 		
 	}
-	
-	private List<Category> parseJSONArray(JSONArray jsonArray) {
 
-		//TODO
-		return null;
-
-	}
 }
