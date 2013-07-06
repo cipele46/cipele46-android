5be7718a58c6e2f719a048fe545fa060c69c87ad
Ivan Ferdelja <ivan.ferdelja@gmail.com>
2013-07-06 09:33:55 +0200

    Updated API.

--- src/eu/fiveminutes/cipele46/api/CipeleAPI.java
+++ src/eu/fiveminutes/cipele46/api/CipeleAPI.java
@@ -1,8 +1,18 @@
 package eu.fiveminutes.cipele46.api;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class CipeleAPI {
 
 	private static CipeleAPI cipele;
+	private RequestQueue reqQueue;
+	
+	public void init(Context c) {
+		reqQueue = Volley.newRequestQueue(c);
+	}
 	
 	public static CipeleAPI get() {
 		if (cipele == null) {
@@ -10,6 +20,11 @@ public class CipeleAPI {
 		}
 		return cipele;
 	}
+	
+	public void getAds(AdsListener adsListener) {
+		
+	}
+	
 	
 	public void getCategories(CategoriesListener cl) {
 		
